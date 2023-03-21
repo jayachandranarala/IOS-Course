a337f4af1013cfc12a88208263c72bdce5ab3290
jayachandra <41367444+jayachandranarala@users.noreply.github.com>
2023-03-21 10:46:13 -0500

    Add files via upload

---
+++ MyWordGuess/MyWordGuess/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MyWordGuess
+//
+//  Created by Narala,Jayachandra on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var DisplayLbl: UILabel!
+    
+    
+    @IBOutlet weak var HintLbl: UILabel!
+    
+    
+    
+    @IBOutlet weak var LetterEntrd: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var StatusLbl: UILabel!
+    
+    
+    @IBOutlet weak var CheckOutlet: UIButton!
+    
+    
+    @IBOutlet weak var PlayagainOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func CheckAgainBtn(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func PlayAgainBtn(_ sender: Any) {
+    }
+    
+
+    
+    
+    @IBAction func LetterEntrd(_ sender: Any) {
+    }
+    
+    
+    
+}
+
