b28458934946e596833aeaa9304804b2d335897e
jayachandra <41367444+jayachandranarala@users.noreply.github.com>
2023-04-20 20:09:21 -0500

    Add files via upload

---
+++ Narala_UniversityApp/Narala_UniversityApp/UniversitiesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Narala_UniversityApp
+//
+//  Created by Narala,Jayachandra on 4/19/23.
+//
+
+import UIKit
+
+class UniversitiesViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var universitiesTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
